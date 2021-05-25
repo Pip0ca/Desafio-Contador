c3db0367e4994bfeb310f548a04f17929fc1334d
Bruna de Paiva Gonçalves <65919877+Pip0ca@users.noreply.github.com>
2021-05-25 15:53:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let btnCont = document.getElementById('btnCont') //reconhecendo o botão btnCont no Java
-let btnRen = document.getElementById('btnRen')//reconhecendo o botão btnRen no Java
 let secund = document.querySelector('.secund')//reconhecendo a div .secund no Java
 let inicial = document.querySelector('.inicial')//reconhecendo a div inicial no Java
 
-let DataPassada = document.getElementById('date').value 
-
 function contagem(){
+  let btnCont = document.getElementById('btnCont') //reconhecendo o botão btnCont no Java
+  let DataPassada = document.getElementById('date').value 
+  let diaSp = document.getElementById('diaSp')
+  let horaSp = document.getElementById('horaSp')
+  let minSp = document.getElementById('minSp')
+  let segSp = document.getElementById('segSp')
+
   inicial.style.display = 'none'
   secund.style.display = 'block'
   // Ao clicar em contagem o div inicial fica invisivel e a div secund fica visivel
@@ -19,12 +22,14 @@ function contagem(){
   var difHour = Math.floor(difTempo/(1000*3600))
   var difMin = Math.floor(difTempo/(1000*60))
   var difSeg = Math.floor(difTempo/(1000))
-  var array_total = [difDias, (disHour%24), (difMin%60), (difSeg%60)]
+  
  
 
 }
 
 function reninciar(){
+  let btnRen = document.getElementById('btnRen')//reconhecendo o botão btnRen no Java
+
   inicial.style.display = 'block'
   secund.style.display = 'none'
   // Ao clicar em contagem o div inicial fica visivel e a div secund fica invisivel
